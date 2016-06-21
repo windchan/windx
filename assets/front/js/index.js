54f0a104f3a24f66caa83fbb550385c4c5224b8b
windchan <v20080443@163.com>
2016-06-21 14:12:50 +0800

    none

--- assets/front/js/index.js
+++ assets/front/js/index.js
@@ -3,3 +3,10 @@ $('#test-img').click(function(){
 });
 
 var bus = BUS('5768a5a655d92bad7d0ea6e3', '33bf8926fea4a2a73221822f519ab2412088220d');
+
+var collection = bus.Collection('sports');
+var obj = collection.create_obj({'monday': 'soccer'});
+obj.set('tuesday', 'badminton');
+obj.save().then(function(result) {
+	console.log(result);
+}, function(err) {console.log(err);});
